3ed4b4f3829995b1bf871ba8dabb87ad2b55809b
adityaharale01 <haraleaditya3@gmail.com>
2026-04-17 00:54:36 +0530

    Added download model script

---
+++ backend/download_model.py
@@ -0,0 +1,4 @@
+import gdown
+
+gdown.download("https://drive.google.com/uc?id=1pDQXeAi_5zSJsxtiST5ZbtfKYGWj6jIf", "skin_type_model.h5", fuzzy=True)
+gdown.download("https://drive.google.com/uc?id=13ZJontYG8vBuhF8TjhqTUVG-5kCRWQ28", "face_shape_model.h5", fuzzy=True)
